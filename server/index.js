ce64ada0c5d2f6470ddc3380ffe6102569b53b3a
MatheusGoetz <matheusgoetz@gmail.com>
2024-03-13 10:49:25 -0300

    criado a rota /summary

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 import cors from 'cors'
+import express from 'express'
+
+const app = express();
+app.use(cors());
+
+app.get('/summary', (req, res) => {
+    res.send('Server on-line');
+});
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
